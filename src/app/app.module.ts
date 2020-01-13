9ce76e600cdccfa24c1fda79d5476e9c2d591ef4
Dheeraj Duggempudi <dheeraj.duggempudi@gmail.com>
2020-01-13 10:36:19 +0530

    untill services

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { HeroService } from './services/hero.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+    
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
